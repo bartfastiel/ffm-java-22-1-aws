4356a725ace3f5d7045ebf22176408f6f3dca562
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-11-10 11:28:55 +0100

    Setup backend and frontend

---
+++ backend/src/test/java/de/neuefische/ffmjava221/aws/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.ffmjava221.aws.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
