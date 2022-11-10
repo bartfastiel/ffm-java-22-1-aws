3b7c00610a8f10fb755cefed0e9b89e2ad44c210
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-11-10 11:34:21 +0100

    Example endpoint + frontend

---
+++ backend/src/main/java/de/neuefische/ffmjava221/aws/backend/FishController.java
@@ -0,0 +1,18 @@
+package de.neuefische.ffmjava221.aws.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/fish")
+@RequiredArgsConstructor
+public class FishController {
+
+    @GetMapping
+    String get() {
+        return "neuefische";
+    }
+
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [fish, setFish] = React.useState<string>();
+
+    useEffect(() => {
+        axios.get('/api/fish')
+            .then(response => response.data)
+            .then(data => setFish(data));
+    }, []);
+
+    return <>{fish}</>;
 }
 
 export default App;
